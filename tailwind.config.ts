7c8a289cc56a613993e67ffbf0b47c2f54174244
Falk33n <tim.falk00@gmail.com>
2024-12-04 10:46:58 +0100

    chore: setup project

---
+++ tailwind.config.ts
@@ -0,0 +1,10 @@
+import type { Config } from 'tailwindcss';
+
+const config = {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {}
+	},
+	plugins: []
+} satisfies Config;
+export default config;
